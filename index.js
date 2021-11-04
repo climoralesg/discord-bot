9a3d7ab882ba62ed9a239241354d4834e3fa5a6e
Claudio <climoralesg@gmail.com>
2021-11-04 00:20:28 -0300

    feature: deploy command example

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
-require('dotenv').config();
-const axios=require('axios').default;
+require("dotenv").config();
+const axios = require("axios").default;
 
-const { Client, Intents } = require('discord.js');
+const { Client, Intents } = require("discord.js");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-const prefix="/";
+client.once("ready", () => {
+  // Comprueba la conexión
+  console.log("Hola! he vuelto");
+});
 
-const lang="es";
-const unit="metric";
+const prefix = "/";
 
-client.on("message",function(message){
-    if(message.author.bot)return; //comprueba si el mensaje enviado es un bot
-    if(!message.content.startsWith(prefix))return; //comprueba si la linea que esta ejecutando sea con el prefijo mencionado anteriormente
+const lang = "es";
+const unit = "metric";
 
-    const commandBody=message.content.slice(prefix.length);//Del mensaje saca el /
-    
-    const args=commandBody.split(' ');//separa el mensaje en un array siendo el ' ' el separador de cada uno
-    
-    const command=args.shift().toLowerCase(); //Elimina el elemento 0, retornando ese dato, los otros datos cambian de posicion
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-    let comuna=args[0].charAt(0).toUpperCase()+args[0].slice(1);
+  const { commandName } = interaction;
 
-    console.log("Comuna",comuna);
+  if (commandName === "ping") {
+    await interaction.reply("has dado ping");
+  }
+  if(commandName==="server"){
+    await interaction.reply(`Has dado a Server: ${interaction.guild.name}\n Creado el: ${interaction.guild.createdAt}`);
+  }
+});
+/*
+client.on("message", function (message) {
+  if (message.author.bot) return; //comprueba si el mensaje enviado es un bot
+  if (!message.content.startsWith(prefix)) return; //comprueba si la linea que esta ejecutando sea con el prefijo mencionado anteriormente
 
-    if(command!=""){
-        
-        if(command=="temperatura"){
-            axios({
-                method:'get',
-                url:'https://api.openweathermap.org/data/2.5/weather?',
+  const commandBody = message.content.slice(prefix.length); //Del mensaje saca el /
 
-                responseType:'json',
-                
-                params:{
-                    q:comuna,
-                    lang:lang,
-                    units:unit,
-                    appid:process.env.API_TOKEN_OPENWEATHER
-                }
-               
-            }).then(function(response){
-                console.log(response.data.main.temp);
-                message.reply(`La temperatura de ${comuna} es de ${response.data.main.temp} °C`);
-            })
-        
-        }
-    }    
-})
+  const args = commandBody.split(" "); //separa el mensaje en un array siendo el ' ' el separador de cada uno
 
+  const command = args.shift().toLowerCase(); //Elimina el elemento 0, retornando ese dato, los otros datos cambian de posicion
+
+  let comuna = args[0].charAt(0).toUpperCase() + args[0].slice(1);
+
+  console.log("Comuna", comuna);
+
+  if (command != "") {
+    if (command == "temperatura") {
+      axios({
+        method: "get",
+        url: "https://api.openweathermap.org/data/2.5/weather?",
+
+        responseType: "json",
+
+        params: {
+          q: comuna,
+          lang: lang,
+          units: unit,
+          appid: process.env.API_TOKEN_OPENWEATHER,
+        },
+      }).then(function (response) {
+        console.log(response.data.main.temp);
+        message.reply(
+          `La temperatura de ${comuna} es de ${response.data.main.temp} °C`
+        );
+      });
+    }
+  }
+});
+*/
 client.login(process.env.API_TOKEN_DISCORD);
-
-
-
-
-
